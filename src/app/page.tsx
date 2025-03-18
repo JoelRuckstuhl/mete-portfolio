eb25064c1f9c28ce8f1ea831a20128d52a07b254
Joel <joel.ruckstuhl@talents.jomb.ch>
2025-03-18 16:45:51 +0100

    sigmav3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,7 @@ import { Header } from "@/components/Header";
 
 const EVENTS_QUERY = defineQuery(`*[
   _type == "event"
-  
-]{ Name, Video, date}|order(date desc)`);
+]{ Name, Video, Description}`);
 
 export default async function IndexPage() {
   const { data: events } = await sanityFetch({ query: EVENTS_QUERY });
@@ -13,12 +12,30 @@ export default async function IndexPage() {
   return (
     <>
       <Header />
-      <main className="flex bg-gray-100 min-h-screen flex-col p-24 gap-12">
-        <h1 className="text-4xl font-bold tracking-tighter">Events</h1>
-        <ul className="grid grid-cols-1 gap-12 lg:grid-cols-2">
-          <div> {events[0].Name}</div>
-          <div>mete</div>
-        </ul>
+
+      <main className="flex bg-gray-100 min-h-screen flex-col p-24">
+        {/* Mapping through the events data */}
+        <div className="flex flex-wrap justify-center gap-40">
+          {" "}
+          {/* Use flex-wrap and gap */}
+          {events.map((event, index) => (
+            <div key={index} className="flex-shrink-0">
+              {" "}
+              {/* Prevent shrinking */}
+              <div>{event.Name}</div>
+              <iframe
+                width="560"
+                height="315"
+                src={event.Video}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
+            </div>
+          ))}
+        </div>
       </main>
     </>
   );
