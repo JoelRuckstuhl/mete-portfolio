5ce8b898f0a1c73cb19c3363cd8e059fb088db4e
Joel <joel.ruckstuhl@talents.jomb.ch>
2025-03-18 14:32:23 +0100

    Sigmav2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <header className="flex w-[100%] h-[110px] justify-around">
+        <div className="w-[80px]">
+          <img src="/turkey.png" alt="Logo" />
+        </div>
+        <nav className="flex">
+          <ul className="flex gap-[30px]">
+            <li>Home</li>
+            <li>About</li>
+            <li>Portfolio</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 };
